3e2229e65332fde548b60534ef6ad10b0e9a6f6c
dannyferc <dannyferc@gmail.com>
2015-02-13 15:41:59 +0100

    Shiny app
    Corrected comments

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 #data processing libraries
 library(XLConnect)
@@ -25,62 +26,59 @@ discipline <- sort(unique(medals$Sport))
 
 
 shinyServer(
-  function(input, output) {
+	function(input, output) {
   
-	#Define and initialize reactive values
-  values <- reactiveValues()
-	values$discipline <- discipline
+		#Define and initialize reactive values
+		values <- reactiveValues()
+		values$discipline <- discipline
 	
-	#Create a checkbox for disciplines
-	output$disciplineControls <- renderUI({
-    checkboxGroupInput('discipline', 'Choose olympic discipline', discipline)
-  })
-  
-  #Create a barplot, reative to input$olympic & input$discipline
-  output$mytable1 <- renderDataTable({
-    library(ggplot2)
-    if (input$medalCategory2 == "gold") {
-      medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Gold.Medals != 0,c(1,2,3,4)]
-    } else {
-      if (input$medalCategory2 == "silver") {
-        medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Silver.Medals != 0,c(1,2,3,5)]
-      } else {
-        if (input$medalCategory2 == "bronze") {
-          medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Bronze.Medals != 0,c(1,2,3,6)]
-        } else {
-          if (input$medalCategory2 == "total") {
-            medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Total.Medals != 0,c(1,2,3,7)]
-          }
-        }      
-      }
-    }
-  })
-  
-	#Create a large table, reative to input$olympic & input$discipline
-	output$myplot1 <- renderPlot({
-	  library(ggplot2)
-    if (input$medalCategory1 == "gold") {
-      medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Gold.Medals != 0,c(1,2,3,4)]
-      print(ggplot(medals1, aes(x = Country, y = Gold.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)))
-    } else {
-	    if (input$medalCategory1 == "silver") {
-	      medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Silver.Medals != 0,c(1,2,3,5)]
-	      print(ggplot(medals1, aes(x = Country, y = Silver.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)))
-	    } else {
-        if (input$medalCategory1 == "bronze") {
-          medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Bronze.Medals != 0,c(1,2,3,6)]
-          print(ggplot(medals1, aes(x = Country, y = Bronze.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)) )
-        } else {
-          if (input$medalCategory1 == "total") {
-            medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Total.Medals != 0,c(1,2,3,7)]
-            print(ggplot(medals1, aes(x = Country, y = Total.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)))
-          }
-        }      
-	    }
-	  }
-	})
-  
-  }
+		#Create a checkbox for disciplines
+		output$disciplineControls <- renderUI({
+			checkboxGroupInput('discipline', 'Choose Olympic discipline', discipline)
+		})
+  	  
+		#Create a barplot, reactive to input$olympic & input$discipline
+		output$myplot1 <- renderPlot({
+			if (input$medalCategory1 == "gold") {
+				medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Gold.Medals != 0,c(1,2,3,4)]
+				print(ggplot(medals1, aes(x = Country, y = Gold.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)))
+			} else {
+				if (input$medalCategory1 == "silver") {
+					medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Silver.Medals != 0,c(1,2,3,5)]
+					print(ggplot(medals1, aes(x = Country, y = Silver.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)))
+				} else {
+					if (input$medalCategory1 == "bronze") {
+						medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Bronze.Medals != 0,c(1,2,3,6)]
+					print(ggplot(medals1, aes(x = Country, y = Bronze.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)) )
+					} else {
+						if (input$medalCategory1 == "total") {
+							medals1 <- medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Total.Medals != 0,c(1,2,3,7)]
+							print(ggplot(medals1, aes(x = Country, y = Total.Medals, fill = Sport)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = -45, hjust = 0)))
+						}
+					}      
+				}
+			}
+		})
+		
+		#Create a table, reactive to input$olympic & input$discipline
+		output$mytable1 <- renderDataTable({
+			if (input$medalCategory2 == "gold") {
+				medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Gold.Medals != 0,c(1,2,3,4)]
+			} else {
+				if (input$medalCategory2 == "silver") {
+					medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Silver.Medals != 0,c(1,2,3,5)]
+				} else {
+					if (input$medalCategory2 == "bronze") {
+						medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Bronze.Medals != 0,c(1,2,3,6)]
+					} else {
+						if (input$medalCategory2 == "total") {
+							medals[medals$Year==input$olympic & medals$Sport %in% input$discipline & medals$Total.Medals != 0,c(1,2,3,7)]
+						}
+					}      
+				}
+			}
+		})
+	}
 )
 
 
